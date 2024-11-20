0cc8adaf38f4c72e4e9af620b9f67f82cd7fcd98
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-20 16:28:03 -0300

    criando respostas

--- pages/api/bancoDeQuestoes.ts
+++ pages/api/bancoDeQuestoes.ts
@@ -2,11 +2,17 @@ import QuestaoModel from "../../model/questao";
 import RespostaModel from "../../model/resposta";
 
 const questoes: QuestaoModel[] = [
-    new QuestaoModel(306, 'qual bixo transmite a doença de chagas', [
+    new QuestaoModel(306, 'Qual bixo transmite a doença de chagas', [
         RespostaModel.errada('Abelha'),
         RespostaModel.errada('Barata'),
         RespostaModel.errada('Pulga'),
         RespostaModel.certa('Barbeiro'),
+    ]),
+    new QuestaoModel(202, 'Qual fruto é conhecido no Norte e Nordeste como "Jerimum".', [
+        RespostaModel.errada('Caju'),
+        RespostaModel.errada('coco'),
+        RespostaModel.errada('Chuchu'),
+        RespostaModel.certa('Abóbora'),
     ])
 ]
 
