a8da8e16bcabbea90ef20718106c82683cc44764
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-21 16:06:41 -0300

    curso de nextjs coder: convertendo pra objeto

--- model/resposta.ts
+++ model/resposta.ts
@@ -1,28 +1,33 @@
-export default class RespostaModel{
-    #valor: string
-    #certa: boolean
-    #revelada: boolean
+export default class RespostaModel {
+  #valor: string;
+  #certa: boolean;
+  #revelada: boolean;
 
-    constructor(valor: string, certa: boolean, revelada = false){
-        this.#valor = valor
-        this.#certa = certa
-        this.#revelada = revelada
-    }
-    static certa(valor: string){
-        return new RespostaModel(valor, true)
-
-    }
-    static errada(valor: string){
-        return new RespostaModel(valor, false)
-
-    }
-    get valor(){
-        return this.#valor
-    }
-    get certa(){
-        return this.#certa
-    }
-    get revelada(){
-        return this.#revelada
-    }
+  constructor(valor: string, certa: boolean, revelada = false) {
+    this.#valor = valor;
+    this.#certa = certa;
+    this.#revelada = revelada;
+  }
+  static certa(valor: string) {
+    return new RespostaModel(valor, true);
+  }
+  static errada(valor: string) {
+    return new RespostaModel(valor, false);
+  }
+  get valor() {
+    return this.#valor;
+  }
+  get certa() {
+    return this.#certa;
+  }
+  get revelada() {
+    return this.#revelada;
+  }
+  paraObjeto() {
+    return {
+      valor: this.#valor,
+      certa: this.#revelada,
+      revelada: this.#revelada,
+    };
+  }
 }
