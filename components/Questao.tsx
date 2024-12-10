b9ff4220e2dfebb14766bbe796b0d5b17d40c30e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-10 17:38:54 -0300

    componente resposta #1

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,16 +1,29 @@
-import styles from '../styles/Questao.module.css'
+import styles from "../styles/Questao.module.css";
 import QuestaoModel from "../model/questao";
-import Enunciado from './Enunciado';
+import Enunciado from "./Enunciado";
+import Resposta from "../components/resposta";
 
 interface QuestaoProps {
-    valor: QuestaoModel
+  valor: QuestaoModel;
 }
-export default function Questao(props:QuestaoProps){
-    const questao = props.valor
-    return (
-        <div className={styles.questao}>
-         <Enunciado texto={questao.enunciado}/>
-        </div>
-    )
-
+export default function Questao(props: QuestaoProps) {
+  const questao = props.valor;
+  function renderizarRespostas() {
+    return questao.respostas.map((resposta, index) => {
+      return (
+        <Resposta
+          valor={resposta}
+          indice={index}
+          letra="A"
+          corLetra="#f2c866"
+        />
+      );
+    });
+  }
+  return (
+    <div className={styles.questao}>
+      <Enunciado texto={questao.enunciado} />
+      {renderizarRespostas()}
+    </div>
+  );
 }
--- components/resposta.tsx
+++ components/resposta.tsx
@@ -7,14 +7,18 @@ interface RespostaProps {
   letra: string;
   corLetra: string;
 }
-export default function Resposta(props) {
+export default function Resposta(props: RespostaProps) {
+  const resposta = props.valor
   return (
     <>
       <div className={styles.resposta}>
         <div className={styles.conteudoResposta}>
           <div className={styles.frente}>
             <div className={styles.letra}>
-
+              {props.letra}
+            </div>
+            <div className={styles.valor}>
+              {resposta.valor}
             </div>
           </div>
           <div className={styles.verso}>
