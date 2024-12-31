8d9ca412f81a7410b1e2b07f6b71280cfba0d864
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-31 15:27:49 -0300

    feat: Componente questionario 1#

--- components/Questinonario.tsx
+++ components/Questinonario.tsx
@@ -1,9 +1,14 @@
+import QuestaoModel from "../model/questao"
+import styles from '../styles/Questinonario.module.css'
  interface QuestinonarioProps{
-
+    questao: QuestaoModel
+    ultima: boolean
+    questaoRespondida: (questao: QuestaoModel) => void
+    inPraProximoPasso: () => void
  }
 export default function Questinonario(props: QuestinonarioProps){
     return (
-        <div>
+        <div className={styles.questionario}>
 
         </div>
     )
