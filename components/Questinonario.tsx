6e4de86bb1bed78f86d805fae50b1ec2d630de08
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-01 15:52:27 -0300

    feat: Componente questionario 1#

--- components/Questinonario.tsx
+++ components/Questinonario.tsx
@@ -1,15 +1,32 @@
-import QuestaoModel from "../model/questao"
-import styles from '../styles/Questinonario.module.css'
- interface QuestinonarioProps{
-    questao: QuestaoModel
-    ultima: boolean
-    questaoRespondida: (questao: QuestaoModel) => void
-    inPraProximoPasso: () => void
- }
-export default function Questinonario(props: QuestinonarioProps){
-    return (
-        <div className={styles.questionario}>
+import QuestaoModel from "../model/questao";
+import styles from "../styles/Questinonario.module.css";
+import Botao from "./Botao";
+import Questao from "./Questao";
+interface QuestinonarioProps {
+  questao: QuestaoModel;
+  ultima: boolean;
+  questaoRespondida: (questao: QuestaoModel) => void;
+  inPraProximoPasso: () => void;
+}
+export default function Questinonario(props: QuestinonarioProps) {
+  function respostaFornecida(indece: number) {}
+  return (
+    <div className={styles.questionario}>
+      {props.questao ? (
+        <Questao
+          valor={props.questao}
+          tempoPraResposta={6}
+          respostaFornecida={respostaFornecida}
+          tempoEsgotado={props.inPraProximoPasso}
+        />
+      ) : (
+        false
+      )}
 
-        </div>
-    )
+      <Botao
+        onClick={props.inPraProximoPasso}
+        texto={props.ultima ? "Finalizar" : "Proxima"}
+      />
+    </div>
+  );
 }
