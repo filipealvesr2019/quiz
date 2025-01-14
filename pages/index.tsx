16045f013b1dc30ef5cdf425aea2fa76505425a3
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-14 15:07:46 -0300

    index do componente #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,77 @@
 import { useEffect, useState } from "react";
-import Questao from "../components/Questao";
 import QuestaoModel from "../model/questao";
-import RespostaModel from "../model/resposta";
-import Botao from "../components/Botao";
-import styles from '../styles/Index.module.css'
+
 import Questinonario from "../components/Questinonario";
-
-const questaoMock = new QuestaoModel(1, "Melhor cor", [
-  RespostaModel.errada("Verde"),
-  RespostaModel.errada("Vermelha"),
-  RespostaModel.errada("Azul"),
-  RespostaModel.certa("Preta"),
-]);
+import { useRouter } from "next/router";
 
 
-const BASE_URL = 'http://localhost:3000/api'
+
+const BASE_URL = "http://localhost:3000/api";
 export default function Home() {
-  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
-  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+  const router = useRouter()
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
+  const [questao, setQuestao] = useState<QuestaoModel>();
   const [respostaCertas, setRespostaCertas] = useState<number>(0);
 
-  async function carregarIdsDasQuestoes(){
+  async function carregarIdsDasQuestoes() {
     const resposta = await fetch(`${BASE_URL}/questionario`);
     const idsDasQuestoes = await resposta.json();
-    setIdsDasQuestoes(idsDasQuestoes)
+    setIdsDasQuestoes(idsDasQuestoes);
   }
 
-
-  
-  async function carregarQuestao(idQuestao: number){
+  async function carregarQuestao(idQuestao: number) {
     const resposta = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
     const json = await resposta.json();
-    const novaQuestao = QuestaoModel.criarUsandoObjeto(json)
-    setQuestao(novaQuestao)
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(json);
+    setQuestao(novaQuestao);
   }
 
-  useEffect(() =>{
-    carregarIdsDasQuestoes()
-  }, [])
+  useEffect(() => {
+    carregarIdsDasQuestoes();
+  }, []);
 
-  useEffect(() =>{
-    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
-  }, [idsDasQuestoes])
-  function questaoRespondida(questaoRespondida:QuestaoModel){
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0]);
+  }, [idsDasQuestoes]);
+  function questaoRespondida(questaoRespondida: QuestaoModel) {
     setQuestao(questaoRespondida);
-    const acertou = questaoRespondida.acertou
-    setRespostaCertas(respostaCertas + (acertou ? 1 : 0))
+    const acertou = questaoRespondida.acertou;
+    setRespostaCertas(respostaCertas + (acertou ? 1 : 0));
   }
 
-  function idProximaPergunta(){
-   const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1;
-   return idsDasQuestoes[proximoIndice]
-  }
-  
-  function inPraProximoPasso(){
-    const proximoId = idProximaPergunta()
-
-    proximoId ? irPraProximaQuestao(proximoId) : finalizar(); 
+  function idProximaPergunta() {
+    if(questao){
+      const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1;
+      return idsDasQuestoes[proximoIndice];
+      
+    }
   }
 
+  function inPraProximoPasso() {
+    const proximoId = idProximaPergunta();
 
-  function irPraProximaQuestao(proximoId: number){
-    carregarQuestao(proximoId)
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar();
   }
 
-  
-  function finalizar(){
-    
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestao(proximoId);
+  }
+
+  function finalizar() {
+    router.push({
+      pathname: "/resultado",
+      query: {
+        total: idsDasQuestoes.length,
+        certas: respostaCertas
+      }
+    })
   }
   return (
- 
-      <Questinonario  questao={questao} ultima={false} questaoRespondida={questaoRespondida} inPraProximoPasso={inPraProximoPasso}/>
-  
+    <Questinonario
+      questao={questao}
+      ultima={idProximaPergunta() === undefined}
+      questaoRespondida={questaoRespondida}
+      inPraProximoPasso={inPraProximoPasso}
+    />
   );
 }
